b8b4a96a2da6331aaf63e68a7a3ec668c52216f0
Damir Lutfrakhmanov <60309177+DamirLut@users.noreply.github.com>
2023-01-24 10:19:04 +0500

    get task by id

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -1,46 +1,60 @@
-import { FastifyPluginAsync } from 'fastify';
-import { instanceToPlain } from 'class-transformer';
-import { Exercise } from '../entity/Exercise';
-import { Task } from '../entity/Task';
+import {FastifyPluginAsync} from 'fastify';
+import {instanceToPlain} from 'class-transformer';
+import {Exercise} from '../entity/Exercise';
+import {Task} from '../entity/Task';
 
 export const autoPrefix = '/task';
 
 const TaskRoute: FastifyPluginAsync = async (fastify) => {
-  const { authorize, administratorOnly } = fastify;
+	const {authorize, administratorOnly} = fastify;
 
-  fastify.addHook('onRequest', authorize);
+	fastify.addHook('onRequest', authorize);
 
-  type CreateTaskDto = {
-    Body: {
-      exercise_id: number;
-      type: string;
-    };
-  };
+	type GetTaskDto = {
+		Querystring: {
+			id: number;
+		}
+	}
 
-  fastify.post<CreateTaskDto>('/create', { onRequest: administratorOnly }, async (req, res) => {
-    const { exercise_id, type } = req.body;
+	fastify.get<GetTaskDto>('/', async (req, res) => {
+		const {id} = req.query;
 
-    const exercise = await Exercise.findOne({
-      where: { id: exercise_id },
-      relations: { tasks: true },
-    });
+		const task = await Task.findOne({where: {id}});
 
-    if (!exercise) throw fastify.httpErrors.badRequest(`Exercise not found`);
+		return instanceToPlain(task, {enableCircularCheck: true});
+	});
 
-    const task = Task.create({
-      exercise,
-      name: 'Задача #' + ((await Task.count()) + 1),
-      type
-    });
-    await task.save();
+	type CreateTaskDto = {
+		Body: {
+			exercise_id: number;
+			type: string;
+		};
+	};
 
-    exercise.tasks.push(task);
-    await exercise.save();
+	fastify.post<CreateTaskDto>('/create', {onRequest: administratorOnly}, async (req, res) => {
+		const {exercise_id, type} = req.body;
 
-    return instanceToPlain(task, {
-      enableCircularCheck: true,
-    });
-  });
+		const exercise = await Exercise.findOne({
+			where: {id: exercise_id},
+			relations: {tasks: true},
+		});
+
+		if (!exercise) throw fastify.httpErrors.badRequest(`Exercise not found`);
+
+		const task = Task.create({
+			exercise,
+			name: 'Задача #' + ((await Task.count()) + 1),
+			type
+		});
+		await task.save();
+
+		exercise.tasks.push(task);
+		await exercise.save();
+
+		return instanceToPlain(task, {
+			enableCircularCheck: true,
+		});
+	});
 };
 
 export default TaskRoute;
